132f9cde0a6b040458a776f4ca8926c100548e93
Kumi-H <935garden@gmail.com>
2025-01-15 19:50:57 +0900

    controllerファイル追加

--- apps/backend/src/main/java/soloco/backend/controllers/DemoContoroller.java
+++ apps/backend/src/main/java/soloco/backend/controllers/DemoContoroller.java
@@ -1,6 +1,5 @@
 package soloco.backend.controllers;
 
-// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
