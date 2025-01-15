32e2d0b42550170f95e0ef6cf7648b971ba4cfa7
Kumi-H <935garden@gmail.com>
2025-01-15 19:50:30 +0900

    Firebase初期化設定

--- apps/backend/README.md
+++ apps/backend/README.md
@@ -1,3 +1,4 @@
 ## 使用技術
 * Java 23.0.1
 * Spring Boot 3.4.1
+* Firebase admin
---
+++ apps/backend/src/main/java/soloco/backend/utils/FirebaseClient.java
@@ -0,0 +1,38 @@
+package soloco.backend.utils;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseClient {
+    Logger logger = LoggerFactory.getLogger(FirebaseClient.class);
+
+    @PostConstruct
+    public void initializeFirebase() {
+        
+        try (InputStream serviceAccount = new ClassPathResource("static/config/credential.json").getInputStream()) {
+            
+            if (serviceAccount == null) {
+                throw new IllegalArgumentException("Firebase credential file not found.");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException | IllegalArgumentException e) {
+            logger.error("error: ", e);
+        }
+    }
+}
