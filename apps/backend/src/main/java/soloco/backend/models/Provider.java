34f44f0c6095577a4c81e4d3c46f4787a14e9020
Kumi-H <935garden@gmail.com>
2025-01-19 18:26:34 +0900

    Providerモデル作成

--- apps/backend/src/main/java/soloco/backend/models/Provider.java
+++ apps/backend/src/main/java/soloco/backend/models/Provider.java
@@ -1,5 +1,51 @@
 package soloco.backend.models;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+// import java.util.List;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "providers")
 public class Provider {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private int phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "business_hours")
+    private String businessHours;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
