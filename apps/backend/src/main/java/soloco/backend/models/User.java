2c8144bdc5a3f9fc7f9dfc1308b680e6a696193d
Kumi-H <935garden@gmail.com>
2025-01-19 18:27:00 +0900

    Userモデル作成

--- apps/backend/src/main/java/soloco/backend/models/User.java
+++ apps/backend/src/main/java/soloco/backend/models/User.java
@@ -1,5 +1,41 @@
 package soloco.backend.models;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }        
 }
